04ec441725105d5a38c25581d7be4c92f9d134b0
yuvraj singh <you@example.com>
2020-05-24 14:30:31 +0530

    now add component

---
+++ React with component/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+import Heading from './Heading';
+import List from "./List"
+/***now using date and time*****/
+
+ 
+function App(){
+    return(
+  <>
+  
+ <Heading />
+ <List />
+ <h1 >hello!!
+ </h1>
+ </>
+);
+}
+export default App;
---
+++ React with component/src/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Heading(){
+    return(
+    <div>
+        <h1>
+            hello div
+        </h1>
+    </div>
+    );
+}
+export default Heading;
